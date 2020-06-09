39a9008e5819f15b2de7d2b7bcfd11ac82f1f68c
Felipe A. Azuero M <azuero@protonmail.com>
2020-06-09 14:15:25 +0200

    Updated files for continuous integration.
    - environment file
    - R packages file
    - .envrc

--- utils/r_packages.R
+++ utils/r_packages.R
@@ -1,12 +1,3 @@
 ## R package installation and configuration
 
-install.packages(c("dplyr",
-                   "haven",
-                   "ggplot2",
-                   "gt",
-                   "IRdisplay",
-                   "IRkernel",
-                   "tidyr"),
-                 repos = "https://cloud.r-project.org/")
-
 IRkernel::installspec(name = 'ir40', displayname = 'R 4.0')
